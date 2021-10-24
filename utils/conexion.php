0c1eb708cde9d33eebc38b5ccdb4c35dfe3cea8a
Javi Blasco <90817111+JaviBBenedicto@users.noreply.github.com>
2021-10-25 00:15:40 +0200

    Update conexion.php

--- utils/conexion.php
+++ utils/conexion.php
@@ -1,15 +1,11 @@
 <?php
 
 function conectar(){
-	#$servername = "localhost";
-	#$username = "miaplicacion";
-	#$password = "miaplicacion";
-	#$dbname = "miaplicacion";
-
-	$servername = "bswfhyhurmgadcbjofpa-mysql.services.clever-cloud.com";
-	$username = "u7ihnxsxcwprnh7p";
-	$password = "oc7fN6kLZK8QQuXia0S2";
-	$dbname = "bswfhyhurmgadcbjofpa";
+	
+	$servername = "*************";
+	$username = "****************";
+	$password = "****************";
+	$dbname = "*******************";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	return $conn;
 }
